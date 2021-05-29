e7ae65add857d5011aa7fcd9a9820d8aeac137ef
Daniel Wade <wade.daniel.w@gmail.com>
2021-05-28 18:57:38 -0700

    :recycle: refactored code

--- main.js
+++ main.js
@@ -1,78 +1,148 @@
-const { get } = require('axios');
+// Built-in modules
 const { spawn } = require('child_process');
 const path = require('path');
 
-// Modules to help control application life-cycle
+// Electron modules
 const { app, BrowserWindow, ipcMain } = require('electron');
+
+// Extra modules
 const getPort = require('get-port');
 const isDevMode = require('electron-is-dev');
+const { get } = require('axios');
 
+/**
+ * @description - Shuts down Electron & Flask.
+ * @param {number} port - Port that Flask server is running on.
+ */
 const shutdown = (port) => {
-  get(`http://localhost:${port}/quit`)
-    .then(() => app.quit())
-    .catch(() => app.quit());
+  get(`http://localhost:${port}/quit`).then(app.quit).catch(app.quit);
 };
 
-// Browser window configurations
+/**
+ * @namespace BrowserWindow
+ * @description - Electron browser windows.
+ * @tutorial - https://www.electronjs.org/docs/api/browser-window
+ */
 const browserWindows = {};
 
-// Function to create main window
+/**
+ * @description - Creates main window.
+ * @param {number} port - Port that Flask server is running on.
+ *
+ * @memberof BrowserWindow
+ */
 const createMainWindow = (port) => {
   const { loadingWindow, mainWindow } = browserWindows;
 
   /**
+   * @description - Function to use custom JavaSCript in the DOM.
+   * @param {string} command - JavaScript to execute in DOM.
+   * @param {function} callback - Callback to execute here once complete.
+   * @returns {promise}
+   */
+  const executeOnWindow = (command, callback) => {
+    return mainWindow.webContents
+      .executeJavaScript(command)
+      .then(callback)
+      .catch(console.error);
+  };
+
+  /**
    * If in developer mode, show a loading window while
    * the app and developer server compile.
-  */
-  if(isDevMode) {
-    mainWindow.hide();
+   */
+  if (isDevMode) {
     mainWindow.loadURL('http://localhost:3000');
+    mainWindow.hide();
 
     /**
      * Opening devTools, must be done before dom-ready
      * to avoid occasional error from the webContents
      * object being destroyed.
-    */
-    mainWindow.webContents.openDevTools();
+     */
+    mainWindow.webContents.openDevTools({ mode: 'undocked' });
 
     /**
-     * Destroy loading window once the main
-     * window is ready.
+     * Hide loading window and show main window
+     * once the main window is ready.
      */
-    mainWindow.webContents.on('dom-ready', () => {
-      loadingWindow.destroy();
-      mainWindow.show();
+    mainWindow.webContents.on('did-finish-load', () => {
+      /**
+       * Checks page for errors that may have occurred
+       * during the hot-loading process.
+       */
+      const isPageLoaded = `
+        var isBodyFull = document.body.innerHTML !== "";
+        var isHeadFull = document.head.innerHTML !== "";
+        var isLoadSuccess = isBodyFull && isHeadFull;
+
+        isLoadSuccess || Boolean(location.reload());
+      `;
+
+      /**
+       * @description Updates windows if page is loaded
+       * @param {*} isLoaded
+       */
+      const handleLoad = (isLoaded) => {
+        if (isLoaded) {
+          /**
+           * Keep show() & hide() in this order to prevent
+           * unresponsive behavior during page load.
+           */
+          mainWindow.show();
+          loadingWindow.hide();
+        }
+      };
+
+      /**
+       * Checks if the page has been populated with
+       * React project. if so, shows the main page.
+       */
+      executeOnWindow(isPageLoaded, handleLoad);
     });
-  }
+  } else mainWindow.loadFile(path.join(__dirname, 'build/index.html'));
 
-  // Use build/index.html for production release
-  else mainWindow.loadFile(path.join(__dirname, 'build/index.html'));
+  /**
+   * If using in production, the built version of the
+   * React project will be used instead of localhost.
+   */
 
-  // Set opacity for title on window blur & focus
+  /**
+   * @description - Controls the opacity of title bar on focus/blur.
+   * @param {number} value - Opacity to set for title bar.
+   */
   const setTitleOpacity = (value) => `
     if(document.readyState === 'complete') {
-      document.getElementById('electron-window-title-text').style.opacity = ${value};
-      document.getElementById('electron-window-title-buttons').style.opacity = ${value};
+      const titleBar = document.getElementById('electron-window-title-text');
+      const titleButtons = document.getElementById('electron-window-title-buttons');
+
+      if(titleBar) titleBar.style.opacity = ${value};
+      if(titleButtons) titleButtons.style.opacity = ${value};
     }
   `;
 
-  // Set window event handlers
-  const executeOnWindow = (command) => mainWindow.webContents.executeJavaScript(command);
   mainWindow.on('focus', () => executeOnWindow(setTitleOpacity(1)));
-  mainWindow.on('blur', () => executeOnWindow(setTitleOpacity(.5)));
+  mainWindow.on('blur', () => executeOnWindow(setTitleOpacity(0.5)));
 
-  // Send window control event listeners to front end
-  ipcMain.on('app-maximize', () => mainWindow.maximize());
-  ipcMain.on('app-minimize', () => mainWindow.minimize());
-  ipcMain.on('app-quit', () => shutdown(port));
-  ipcMain.on('app-unmaximize', () => mainWindow.unmaximize());
-  ipcMain.on('get-port-number', (event) => (event.returnValue = port));
+  /**
+   * Listen and respond to ipcRenderer events on the frontend.
+   * @see `src\utils\services.js`
+   */
+  ipcMain.on('app-maximize', (_event, _arg) => mainWindow.maximize());
+  ipcMain.on('app-minimize', (_event, _arg) => mainWindow.minimize());
+  ipcMain.on('app-quit', (_event, _arg) => shutdown(port));
+  ipcMain.on('app-unmaximize', (_event, _arg) => mainWindow.unmaximize());
+  ipcMain.on('get-port-number', (event, _arg) => (event.returnValue = port));
 };
 
-// Loading window to show while Dev Build is being created
+/**
+ * @description - Creates loading window to show while build is created.
+ * @memberof BrowserWindow
+ */
 const createLoadingWindow = () => {
   return new Promise((resolve, reject) => {
     const { loadingWindow } = browserWindows;
+
     const loaderConfig = {
       react: 'utilities/loaders/react/index.html',
       redux: 'utilities/loaders/redux/index.html'
@@ -80,11 +150,12 @@ const createLoadingWindow = () => {
 
     try {
       loadingWindow.loadFile(path.join(__dirname, loaderConfig.react));
+
       loadingWindow.webContents.on('did-finish-load', () => {
         resolve(loadingWindow.show());
       });
-    } catch(error) {
-      reject((error) => console.error(error));
+    } catch (error) {
+      reject(console.error(error));
     }
   });
 };
@@ -93,15 +164,20 @@ const createLoadingWindow = () => {
  * This method will be called when Electron has finished
  * initialization and is ready to create browser windows.
  * Some APIs can only be used after this event occurs.
-*/
+ */
 app.whenReady().then(async () => {
-
-  // Method to set port in range of 3001-3999, based on availability
+  /**
+   * Method to set port in range of 3001-3999,
+   * based on availability.
+   */
   const port = await getPort({
     port: getPort.makeRange(3001, 3999)
   });
 
-  // Initialize main browser window
+  /**
+   * Assigns the main browser window on the
+   * browserWindows object.
+   */
   browserWindows.mainWindow = new BrowserWindow({
     frame: false,
     height: 370,
@@ -111,39 +187,57 @@ app.whenReady().then(async () => {
     minWidth: 675,
     maxWidth: 675,
     webPreferences: {
+      contextIsolation: false,
       enableRemoteModule: true,
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js')
     }
   });
 
-  // If dev mode, use loading window and run Flask in shell
-  if(isDevMode) {
-    browserWindows.loadingWindow = new BrowserWindow({ frame: false }),
-    createLoadingWindow().then(()=> createMainWindow(port));
-    spawn(`python app.py ${port}`, { detached: true, shell: true, stdio: 'inherit' });
-  }
-
-  // Connect to Python micro-services for production.
-  else {
+  /**
+   * If not using in production, use the loading window
+   * and run Flask in shell.
+   */
+  if (isDevMode) {
+    (browserWindows.loadingWindow = new BrowserWindow({ frame: false })),
+      createLoadingWindow().then(() => createMainWindow(port));
+    spawn(`python app.py ${port}`, {
+      detached: true,
+      shell: true,
+      stdio: 'inherit'
+    });
+  } else {
+    /**
+     * If using in production, use the main window
+     * and run bundled app.exe file.
+     */
     createMainWindow(port);
-    spawn(`start ./resources/app/app.exe ${port}`, { detached: false, shell: true, stdio: 'pipe' });
+    spawn(`start ./resources/app/app.exe ${port}`, {
+      detached: false,
+      shell: true,
+      stdio: 'pipe'
+    });
   }
 
   app.on('activate', () => {
     /**
      * On macOS it's common to re-create a window in the app when the
      * dock icon is clicked and there are no other windows open.
-    */
+     */
     if (BrowserWindow.getAllWindows().length === 0) createMainWindow(port);
   });
 
-  // Feature to ensure single app instance
+  /**
+   * Ensures that only a single instance of the app
+   * can run, this correlates with the "name" property
+   * used in `package.json`.
+   */
   const initialInstance = app.requestSingleInstanceLock();
   if (!initialInstance) app.quit();
   else {
     app.on('second-instance', () => {
-      if(browserWindows.mainWindow?.isMinimized()) browserWindows.mainWindow?.restore();
+      if (browserWindows.mainWindow?.isMinimized())
+        browserWindows.mainWindow?.restore();
       browserWindows.mainWindow?.focus();
     });
   }
@@ -152,7 +246,7 @@ app.whenReady().then(async () => {
    * Quit when all windows are closed, except on macOS. There, it's common
    * for applications and their menu bar to stay active until the user quits
    * explicitly with Cmd + Q.
-  */
+   */
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') shutdown(port);
   });
--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,6 @@
 const { spawn } = require('child_process');
-const spawnOptions = { detached: false, shell: true, stdio: 'inherit' };
+const getPort = require('get-port');
+const { get } = require('axios');
 
 /**
  * @namespace Starter
@@ -11,9 +12,52 @@ class Starter {
    * @description - Starts developer mode.
    * @memberof Starter
    */
-  developerMode = () => {
-    spawn(`cross-env BROWSER=none react-scripts start`, spawnOptions);
-    spawn('electron .', spawnOptions);
+  developerMode = async () => {
+
+    // Child spawn options for console
+    const spawnOptions = {
+      hideLogs: { detached: false, shell: true, stdio: 'pipe' },
+      showLogs: { detached: false, shell: true, stdio: 'inherit' }
+    };
+
+    /**
+     * Method to get first port in range of 3001-3999,
+     * Remains unused here so will be the same as the
+     * port used in main.js
+     */
+    const port = await getPort({
+      port: getPort.makeRange(3001, 3999)
+    });
+
+    // Kill anything that might using required React port
+    spawn(`npx kill-port 3000`, spawnOptions.hideLogs);
+
+    // Start & identify React & Electron processes
+    spawn(`cross-env BROWSER=none react-scripts start`, spawnOptions.showLogs);
+    spawn('electron .', spawnOptions.showLogs);
+
+    // Kill processes on exit
+    const exitOnEvent = (event) => {
+      process.once(event, () => {
+        try {
+
+          // These errors are expected since the connection is closing
+          const expectedErrors = ['ECONNRESET', 'ECONNREFUSED'];
+
+          // Send command to Flask server to quit and close
+          get(`http://localhost:${port}/quit`)
+            .catch((error) => !expectedErrors.includes(error.code) && console.log(error));
+
+        } catch(error) {
+
+          // This errors is expected since the process is closing
+          if(error.code !== 'ESRCH') console.error(error);
+        }
+      });
+    };
+
+    const exitEvents = ['exit', 'SIGINT', 'SIGUSR1', 'SIGUSR2', 'uncaughtException', 'SIGTERM'];
+    exitEvents.forEach(exitOnEvent);
   };
 
 }
