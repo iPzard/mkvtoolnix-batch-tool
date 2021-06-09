2419a3674b2d8a3461c637233f9a617509191ba3
Daniel Wade <wade.daniel.w@gmail.com>
2021-06-08 22:02:25 -0700

    :recycle: refactored code

--- app.py
+++ app.py
@@ -26,20 +26,20 @@ socketioConfig = {"async_mode": "threading"}
 # Developer mode uses app.py
 if "app.py" in sys.argv[0]:
 
-    # Update app config
-    app_config["debug"] = True
+  # Update app config
+  app_config["debug"] = True
 
-    # CORS settings
-    cors = CORS(
-        app,
-        resources={r"/*": {"origins": "http://localhost*"}},
-    )
+  # CORS settings
+  cors = CORS(
+    app,
+    resources={r"/*": {"origins": "http://localhost*"}},
+  )
 
-    # SocketIO CORS config
-    socketioConfig["cors_allowed_origins"] = "*"
+  # SocketIO CORS config
+  socketioConfig["cors_allowed_origins"] = "*"
 
-    # CORS headers
-    app.config["CORS_HEADERS"] = "Content-Type"
+  # CORS headers
+  app.config["CORS_HEADERS"] = "Content-Type"
 
 
 """
@@ -58,108 +58,108 @@ files using user-provided options.
 @app.route("/process_batch", methods=["POST"])
 def process_batch():
 
-    # REST call request body
-    input_directory = request.json["input"]
-    output_directory = request.json["output"]
-    settings = request.json["settings"]
+  # REST call request body
+  input_directory = request.json["input"]
+  output_directory = request.json["output"]
+  settings = request.json["settings"]
 
-    # User settings from request body
-    is_remove_ads = settings["isRemoveAds"]
-    is_remove_existing_subtitles = settings["isRemoveExistingSubtitles"]
-    is_remove_old = settings["isRemoveOld"]
-    is_remove_subtitles = settings["isRemoveSubtitles"]
-    is_same_as_source = settings["isSameAsSource"]
-    language = settings["language"]
+  # User settings from request body
+  is_remove_ads = settings["isRemoveAds"]
+  is_remove_existing_subtitles = settings["isRemoveExistingSubtitles"]
+  is_remove_old = settings["isRemoveOld"]
+  is_remove_subtitles = settings["isRemoveSubtitles"]
+  is_same_as_source = settings["isSameAsSource"]
+  language = settings["language"]
 
-    # Get batch of files to process
-    batch_data = FileWalker.get_files(input_directory, is_remove_subtitles)
-    batch = batch_data["files"]
-    warning = batch_data["warning"]
+  # Get batch of files to process
+  batch_data = FileWalker.get_files(input_directory, is_remove_subtitles)
+  batch = batch_data["files"]
+  warning = batch_data["warning"]
 
-    # Communicate batch details to front end
-    socketio.emit("batch_size", len(batch))
+  # Communicate batch details to front end
+  socketio.emit("batch_size", len(batch))
 
-    # Iterate through array of tuples and merge
-    for video_input_path, subtitle_input_paths in batch:
-        video_name = PurePath(video_input_path).stem
-        video_output_path = original_output_path = os.path.join(
-            output_directory, f"{video_name}"
-        )
-        video_output_extension = ".mkv"
-
-        # If output is "same as source"
-        if is_same_as_source:
-
-            # Update paths to match current video source directory
-            video_output_path = original_output_path = os.path.splitext(
-                video_input_path
-            )[0]
-
-        # Prevent duplicate file names by adding (#) to name
-        count = 1
-        while os.path.exists(video_output_path + video_output_extension):
-            video_output_path = f"{original_output_path} ({count})"
-            count += 1
-
-        # Once final video path is determined, add its extension
-        video_output_path += video_output_extension
-
-        # Communicate that directory is being processed to front end
-        socketio.emit("processing_subdirectory")
-
-        # If "remove" subtitles
-        if is_remove_subtitles:
-            MKVToolNix.remove_subtitles(video_input_path, video_output_path)
-
-        # If "merge" subtitles
-        else:
-            default_language_track = language["text"]
-
-            # Process batch
-            MKVToolNix.add_subtitles(
-                is_remove_ads,
-                is_remove_existing_subtitles,
-                subtitle_input_paths,
-                default_language_track,
-                video_input_path,
-                video_output_path,
-            )
-
-        # If remove old, delete original files and directory (if empty after)
-        if is_remove_old:
-            video_directory = PurePath(video_input_path).parent
-
-            # Delete old video file
-            os.remove(video_input_path)
-
-            # Rename new video file to match old name (now removed)
-            os.rename(video_output_path, f"{original_output_path}.mkv")
-
-            # Delete all old subtitle files
-            if subtitle_input_paths is not None: # e.g., not "remove" mode
-              for subtitle_input_path in subtitle_input_paths:
-                  os.remove(subtitle_input_path)
-
-            # Delete video directory if it's now empty
-            if len(os.listdir(video_directory)) == 0 and not is_same_as_source:
-                os.rmdir(video_directory)
-
-    # Status, warning, and error message handling
-    status = "Batch complete"
-    error = (
-        "No valid directories were found using the path provided."
-        if len(batch) == 0
-        else None
+  # Iterate through array of tuples and merge
+  for video_input_path, subtitle_input_paths in batch:
+    video_name = PurePath(video_input_path).stem
+    video_output_path = original_output_path = os.path.join(
+      output_directory, f"{video_name}"
     )
+    video_output_extension = ".mkv"
 
-    if error is not None:
-        status = "Error"
+    # If output is "same as source"
+    if is_same_as_source:
 
-    elif warning is not None:
-        status = "Warning"
+      # Update paths to match current video source directory
+      video_output_path = original_output_path = os.path.splitext(
+        video_input_path
+      )[0]
 
-    # Return results to the front end
-    return jsonify({"status": status, "warning": warning, "error": error})
+    # Prevent duplicate file names by adding (#) to name
+    count = 1
+    while os.path.exists(video_output_path + video_output_extension):
+      video_output_path = f"{original_output_path} ({count})"
+      count += 1
+
+    # Once final video path is determined, add its extension
+    video_output_path += video_output_extension
+
+    # Communicate that directory is being processed to front end
+    socketio.emit("processing_subdirectory")
+
+    # If "remove" subtitles
+    if is_remove_subtitles:
+      MKVToolNix.remove_subtitles(video_input_path, video_output_path)
+
+    # If "merge" subtitles
+    else:
+      default_language_track = language["text"]
+
+      # Process batch
+      MKVToolNix.add_subtitles(
+        is_remove_ads,
+        is_remove_existing_subtitles,
+        subtitle_input_paths,
+        default_language_track,
+        video_input_path,
+        video_output_path,
+      )
+
+    # If remove old, delete original files and directory (if empty after)
+    if is_remove_old:
+      video_directory = PurePath(video_input_path).parent
+
+      # Delete old video file
+      os.remove(video_input_path)
+
+      # Rename new video file to match old name (now removed)
+      os.rename(video_output_path, f"{original_output_path}.mkv")
+
+      # Delete all old subtitle files
+      if subtitle_input_paths is not None: # e.g., not "remove" mode
+        for subtitle_input_path in subtitle_input_paths:
+          os.remove(subtitle_input_path)
+
+      # Delete video directory if it's now empty
+      if len(os.listdir(video_directory)) == 0 and not is_same_as_source:
+        os.rmdir(video_directory)
+
+  # Status, warning, and error message handling
+  status = "Batch complete"
+  error = (
+    "No valid directories were found using the path provided."
+    if len(batch) == 0
+    else None
+  )
+
+  if error is not None:
+    status = "Error"
+
+  elif warning is not None:
+    status = "Warning"
+
+  # Return results to the front end
+  return jsonify({"status": status, "warning": warning, "error": error})
 
 
 """
@@ -178,8 +178,8 @@ Flask when Electron app closes.
 """
 @app.route("/quit")
 def quit():
-    shutdown = request.environ.get("werkzeug.server.shutdown")
-    return shutdown()
+  shutdown = request.environ.get("werkzeug.server.shutdown")
+  return shutdown()
 
 
 """
@@ -188,4 +188,4 @@ Uses a random port between 3000
 and 3999.
 """
 if __name__ == "__main__":
-    socketio.run(app, **app_config)
+  socketio.run(app, **app_config)
